fa77ab57b490ab50c5a27cf3116c27a876f2d7b6
esterlein <esterlein.git@gmail.com>
2024-06-12 17:10:44 -0400

    caster in progress

--- project/scripts/caster.gd
+++ project/scripts/caster.gd
@@ -2,6 +2,12 @@ class_name Caster
 extends Object
 
 
+var size: int = 10
+var strength: int = 10
+var noise: int = 2
+var density: int = 10
+var sparsity: int = 1
+
 func get_planes() -> Array:
 	pass
 	
