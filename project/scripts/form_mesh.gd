e3a98331e35ad503de746b5bce92aa7d21b54dd5
esterlein <esterlein.git@gmail.com>
2024-06-11 23:38:13 -0400

    add commit mesh

--- project/scripts/form_mesh.gd
+++ project/scripts/form_mesh.gd
@@ -16,4 +16,20 @@ func add_stream(attrs: MeshAttributes) -> void:
 
 
 func commit_mesh() -> void:
-	pass
+	mesh = ArrayMesh.new()
+	
+	mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, self.layers.create_array())
+	
+	var mat = StandardMaterial3D.new()
+	mat.set_albedo(layer_color)
+	mat.set_cull_mode(StandardMaterial3D.CULL_DISABLED)
+
+	set_surface_override_material(0, mat)
+
+	mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, self.streams.create_array())
+	
+	mat = StandardMaterial3D.new()
+	mat.set_albedo(stream_color)
+	mat.set_cull_mode(StandardMaterial3D.CULL_DISABLED)
+
+	set_surface_override_material(1, mat)
