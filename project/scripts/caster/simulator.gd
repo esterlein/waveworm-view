bb490c5db162db99845168f3e3177b23e4eade42
esterlein <esterlein.git@gmail.com>
2024-06-14 23:12:39 -0400

    simulator in progress

--- project/scripts/caster/simulator.gd
+++ project/scripts/caster/simulator.gd
@@ -2,10 +2,14 @@ class_name Simulator
 extends Object
 
 
-var size: int = 10
-var noise: int = 2
-var density: int = 10
-var sparsity: int = 1
-var strength: int = 10
+var _size: int = 10
+var _noise: int = 2
+var _strength: int = 10
 
-var mtx_field
+var _probe_density: int = 10
+var _probe_sparsity: int = 1
+
+var mtx_field: Array[float]
+var mtx_probe: Array[float]
+
+
