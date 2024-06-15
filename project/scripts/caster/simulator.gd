9d27c650376c8c738b34518171e18f798d3e666e
esterlein <esterlein.git@gmail.com>
2024-06-14 23:31:12 -0400

    simulator in progress

--- project/scripts/caster/simulator.gd
+++ project/scripts/caster/simulator.gd
@@ -14,8 +14,8 @@ var mtx_probe: Array[float]
 
 
 func _init():
-	self.mtx_field.resize(self._size ** 3)
-	self.mtx_probe.resize(self._size ** 3)
+	mtx_field.resize(_size ** 3)
+	mtx_probe.resize(_size ** 3)
 
 func set_gradient_sphere() -> void:
 	pass
