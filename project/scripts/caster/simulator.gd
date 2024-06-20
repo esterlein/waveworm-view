c9075d43a7c584fb8f4d59c2af39d1eba5c89aba
esterlein <esterlein.git@gmail.com>
2024-06-19 23:40:03 -0400

    add scalar offset normalization

--- project/scripts/caster/simulator.gd
+++ project/scripts/caster/simulator.gd
@@ -22,12 +22,26 @@ func _init():
 
 
 func set_gradient_sphere() -> void:
-	var center_x = _size / 2
-	var center_y = center_x
-	var center_z = center_x
+	var center_x: float = _size / 2.
+	var center_y: float = center_x
+	var center_z: float = center_x
 	
 	for x in range(_size):
 		for y in range(_size):
 			for z in range(_size):
 				mtx_field[x + y * 10 + z * 100] = float(_strength - sqrt(abs(center_x - x) ** 2 +
 				abs(center_y - y) ** 2 + abs(center_z - z) ** 2)) + randfn(.0, 1.)
+
+
+func scalar_offset_normalize():
+	var min_val: float = 0
+	
+	for x in range(_size):
+		for y in range(_size):
+			for z in range(_size):
+				min_val = min(min_val, mtx_field[x + y * 10 + z * 100])
+	
+	for x in range(_size):
+		for y in range(_size):
+			for z in range(_size):
+				mtx_field[x + y * 10 + z * 100] += -min_val
