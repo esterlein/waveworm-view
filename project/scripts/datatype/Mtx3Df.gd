608359d63c5f1a8fbd4eb2497b7a4e53e2d223f3
esterlein <esterlein.git@gmail.com>
2024-06-26 21:37:12 -0400

    add 3d matrix type

--- project/scripts/datatype/Mtx3Df.gd
+++ project/scripts/datatype/Mtx3Df.gd
@@ -1,4 +1,12 @@
+class_name Mtx3Df
 extends Object
 
 
-ß
+var _size: int = 0
+var _storage: Array[float]
+
+
+func _init(size: int, value: float = 0):
+	self._size = size
+	self._storage.resize(size ** 3)
+	self._storage.fill(value)
