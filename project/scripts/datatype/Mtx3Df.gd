4b23a2a280f1280a6a925886a1f3650fa1a8daf1
esterlein <esterlein.git@gmail.com>
2024-06-27 23:42:54 -0400

    add flat index math

--- project/scripts/datatype/Mtx3Df.gd
+++ project/scripts/datatype/Mtx3Df.gd
@@ -2,23 +2,21 @@ class_name Mtx3Df
 extends Object
 
 
+var _dim: Vector3i
 var _size: int = 0
-var _dims: Vector3i
 var _storage: Array[float]
 
 
-func _init(dims: Vector3i, value: float = .0):
-	self._dims = dims
-	self._size = dims.x * dims.y * dims.z
+func _init(dim: Vector3i, value: float = .0):
+	self._dim = dim
+	self._size = dim.x * dim.y * dim.z
 	self._storage.resize(self._size)
 	self._storage.fill(value)
 
 func coords(index: int) -> Vector3i:
-	return Vector3i(index / _size ** 2, \
-					index / _size ** 2, \
-					index / _size ** 2)
+	return Vector3i()
 
 func index(x: int, y: int, z: int) -> int:
-	pass
+	return x + self._dim.x * (y + self._dim.y * z)
 
 
