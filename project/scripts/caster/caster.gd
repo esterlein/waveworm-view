72a3a115430e0b1b06c55acfd059eb42637ee4c9
esterlein <esterlein.git@gmail.com>
2024-07-24 23:37:23 -0400

    field creation

--- project/scripts/caster/caster.gd
+++ project/scripts/caster/caster.gd
@@ -2,18 +2,17 @@ class_name Caster
 extends RefCounted
 
 
-var size: int = 10
-var strength: int = 10
+var dims := Vector3i(10, 10, 10)
 var noise: int = 2
+var strength: int = 10
 var density: int = 10
 var sparsity: int = 1
 
-func get_radial_gradient() -> Array:
-	pass
-	
-func backtrack():
+
+func _init():
 	pass
 
-func get_planes() -> Array:
-	pass
-
+func create_fields():
+	var field := SimField.new(dims, noise, strength, density, sparsity)
+	field.mtx_field = Simulator.get_gradient_sphere(field.dims, field.strength)
+	field.mtx_probe = Simulator.get_probe(field)
