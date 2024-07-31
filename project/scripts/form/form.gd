37f59b94a73d9302af5a118aae572185e40b025a
esterlein <esterlein.git@gmail.com>
2024-07-30 21:22:29 -0400

    add matrix test output

--- project/scripts/form/form.gd
+++ project/scripts/form/form.gd
@@ -3,7 +3,7 @@ extends Node3D
 
 
 var dims := Vector3i(10, 10, 10)
-var noise: int = 2
+var noise: int = 1
 var strength: int = 10
 var density: int = 5
 var sparsity: int = 1
@@ -27,3 +27,17 @@ func generate() -> void:
 	caster = FieldCaster.new(dims, noise, strength, density, sparsity)
 	caster.mtx_field = Simulator.get_gradient_sphere(caster.dims, caster.strength)
 	caster.mtx_probe = Simulator.get_probe(caster)
+	
+	# matrix test output block
+	
+	var pad := 2
+	var mult := 0.01
+	var delim := " "
+	
+	for x in range(caster.dims.x):
+		var plane: String
+		for y in range(caster.dims.y):
+			for z in range(caster.dims.z):
+				plane += str(snappedf(caster.mtx_field.getV(Vector3i(x, y, z)), mult)).pad_decimals(pad) + delim
+			plane += '\n'
+		print(plane + '\n')
